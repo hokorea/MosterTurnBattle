10f917952ad417d2f67920f0b4ed06fcc1c47fd9
hokorea <a01092012710@gmail.com>
2025-12-03 23:18:43 +0900

    feat: extend type definitions in types.h

--- include/types.h
+++ include/types.h
@@ -3,9 +3,9 @@
 
 // 공격자의 타입
 typedef enum ActorType{
-    ACTOR_PLAYER_MON,
-    ACTOR_WILD_MON,
-    ACTOR_ENEMY_TRAINER_MON
+    ACTOR_PLAYER,
+    ACTOR_WILD,
+    ACTOR_ENEMY_TRAINER
 }ActorType;
 
 // 물리/특수/변화
@@ -16,11 +16,37 @@ typedef enum MoveCategory{
 }MoveCategory;
 
 typedef enum Type{
-    TYPE_NORMAL,
-    TYPE_ELECTRIC,
-    TYPE_FLYING,
-    TYPE_GROUND,
-    TYPE_NONE
+    TYPE_NORMAL = 1, // 노말
+    TYPE_FIGHTING,   // 격투
+    TYPE_FLYING,     // 비행
+    TYPE_POISON,     // 독
+    TYPE_GROUND,     // 땅
+    TYPE_ROCK,       // 바위
+    TYPE_BUG,        // 벌레
+    TYPE_GHOST,      // 고스트
+    TYPE_STEEL,      // 강철
+    TYPE_FIRE,       // 불꽃
+    TYPE_WATER,      // 물
+    TYPE_GRASS,      // 풀
+    TYPE_ELECTRIC,   // 전기
+    TYPE_PSYCHIC,    // 에스퍼
+    TYPE_ICE,        // 얼음
+    TYPE_DRAGON,     // 드래곤
+    TYPE_DARK,       // 악
+    TYPE_FAIRY,      // 페어리
+
+    TYPE_NONE        // 무타입 또는 에러 처리용
 }Type;
+
+typedef enum Menu{
+    MENU_BATTLE,
+    MENU_CENTER,
+    MENU_SHOP
+}Menu;
+
+typedef enum BattleStatus{
+    BATTLE_WIN,
+    BATTLE_LOSE,
+}BattleStatus;
 
 #endif
