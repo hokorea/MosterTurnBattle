14f0f3298b65e160d7de93ff0bd09b65b1e9a53c
hokorea <a01092012710@gmail.com>
2025-12-03 23:18:03 +0900

    refactor: split Move and Pokemon into separate headers

---
+++ include/move.h
@@ -0,0 +1,18 @@
+#ifndef MOVE_H
+#define MOVE_H
+
+#define MAX_MOVES 4
+
+typedef struct Move{
+    const char *name; // 기술 이름
+    int power; // 위력
+    MoveCategory category; // 물리/특수/변화
+    int accuracy; // 명중률 (0 ~ 100)
+
+    int max_pp; // 최대 pp(파워포인트 : 기술을 사용할 수 있는 횟수)
+    int pp; // 현재 pp
+
+    Type type; // 타입
+} Move;
+
+#endif
---
+++ include/pokemon.h
@@ -0,0 +1,32 @@
+#ifndef POKEMON_H
+#define POKEMON_H
+
+#include "types.h"
+#include "move.h"
+
+typedef struct Pokemon{
+    const char *name; // 포켓몬 이름
+
+    int level; // 레벨
+    int exp; // 현재 경험치
+
+    int max_hp; // 최대 체력
+    int hp; // 현재 체력
+
+    int atk; // 공격
+    int def; // 방어
+    int sp_atk; // 특수공격
+    int sp_def; // 특수방어
+    int speed; // 속도
+
+    // 단계 필드 (-6 ~ +6)
+    int atk_stage, def_stage, sp_atk_stage, sp_def_stage, 
+    speed_stage, accuracy_stage;
+
+    Type type[2]; // 타입
+
+    Move moves[MAX_MOVES]; // 기술
+    int move_count; // 기술 개수
+} Pokemon;
+
+#endif
